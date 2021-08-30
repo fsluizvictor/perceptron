3f780bce53d2cf2065de57d2e8626ee322c430a2
fsluizvictor <fs.luizvictor@gmail.com>
2021-08-29 23:12:42 -0300

    to creat a function that read csv files

---
+++ src/utils/csv_utils.py
@@ -0,0 +1,11 @@
+import csv
+from typing import List
+
+
+def open_file_csv(file_name: str) -> List[List[str]]:
+    with open(file_name, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        file_rows = list()
+        for row in spamreader:
+            file_rows.append(row)
+    return file_rows
