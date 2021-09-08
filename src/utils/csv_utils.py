d58141ce8b6ab7b16d91c5ca8c475272b7f9f19b
fsluizvictor <fs.luizvictor@gmail.com>
2021-09-08 20:30:49 -0300

    add file with 10000 epochs

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -18,7 +18,7 @@ def open_file_csv(file_name: str) -> List[List[str]]:
 
 def write_file_csv(file_name: str, content: List[str]):
     with open(file_name, 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
         for row in content:
             spamwriter.writerow(row)
 
